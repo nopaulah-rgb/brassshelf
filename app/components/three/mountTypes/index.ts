4f92ae157d4f9894e7932e6ba963bb83d71c053b
Banu Seyhan <95204399+banuseyhan@users.noreply.github.com>
2025-05-30 02:54:58 +0300

    Add index file for mount types exports
    - Create a new index.ts file to consolidate exports for various mount type handlers, including ceiling, wall, and floor configurations.
    - This structure enhances modularity and simplifies the import process for mount type functions in the application.
    - Future mount type handlers can be easily added to this index for improved organization.

---
+++ app/components/three/mountTypes/index.ts
@@ -0,0 +1,8 @@
+export { handleCeilingMount } from "./CeilingMount";
+export { handleWallToCounterMount } from "./WallToCounterMount";
+export { handleWallToFloorMount } from "./WallToFloorMount";
+export { handleWallMount } from "./WallMount";
+export { handleCeilingToWallMount } from "./CeilingToWallMount";
+export { handleCeilingToFloorMount } from "./CeilingToFloorMount";
+export { handleCeilingToCounterMount } from "./CeilingToCounterMount";
+// Export other mount type handlers as they are created 
